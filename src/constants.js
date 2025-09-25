767d8a0a3063927f06fb091208137788cd2fad20
Barış Çetin <bariscetintr@gmail.com>
2025-09-25 10:59:09 +0300

    fix

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-// puts /DIKW/ in front on production builds, and "/" in dev
+// src/constants.js
 export const LOGO_SRC = new URL('logo.png', import.meta.env.BASE_URL).href;
 export const SECTIONS = ["Data", "Information", "Knowledge", "Wisdom"];
